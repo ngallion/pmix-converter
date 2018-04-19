b167dfc3626c333b03fdd59e015a77ba126654b5
Nick Gallion <nicholas.gallion@gmail.com>
2018-04-19 16:01:56 -0500

    Added db functionality with respect to recipes within csv form to add recipes

--- main.py
+++ main.py
@@ -45,12 +45,10 @@ class Ingredient(db.Model):
     __tablename__='ingredient'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
-    quantity = db.Column(db.Float)
     recipes = db.relationship("Association")
 
-    def __init__(self, name, quantity):
+    def __init__(self, name):
         self.name = name
-        self.quantity = quantity
 
 @app.route('/', methods=['POST','GET'])
 def index():
@@ -58,12 +56,61 @@ def index():
 
 @app.route('/csvviewer', methods=['POST','GET'])
 def csvviewer():
-    pmix_to_list(pmix)
-
+    
     return render_template('csvviewer.html',product_list=product_list)
+
+@app.route('/search', methods=['POST'])
+def search():
+    search = request.form['search']
+
+    filtered_product_list = []
+    
+    for product in product_list:
+        if product[0].find(search) != -1:
+            filtered_product_list.append(product)
+    
+    return render_template('csvviewer.html',product_list=filtered_product_list)
+ 
+
+
+
+@app.route('/add-ingredient', methods=['POST'])
+def add_ingredient():
+    ingredient = request.form['ingredient']
+    recipe = request.form['recipe']
+    
+    existing_recipe = Recipe.query.filter_by(name=recipe).first()
+    if not existing_recipe:
+        new_recipe = Recipe(recipe)
+        db.session.add(new_recipe)
+    
+
+    existing_ingredient = Ingredient.query.filter_by(name=ingredient).first()
+    if not existing_ingredient:
+        new_ingredient = Ingredient(ingredient)
+        db.session.add(new_ingredient)
+
+    db.session.commit()
+
+    quantity = request.form['quantity']
+    ingredient_id = Ingredient.query.filter_by(name=ingredient).first().id
+    recipe_id = Recipe.query.filter_by(name=recipe).first().id
+
+    existing_association = Association.query.filter_by(ingredient_id=ingredient_id,recipe_id=recipe_id).first()
+    
+    if not existing_association:
+        new_association = Association(ingredient_id,recipe_id,quantity)
+        db.session.add(new_association)
+        db.session.commit()
+        flash("ingredient added")
+    else:
+        flash("recipe already contains this ingredient")
+
+    return redirect('/csvviewer')
 
 product_list = []
 pmix = "pmix.csv"
+
 def pmix_to_list(pmix):
     with open(pmix, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -72,6 +119,8 @@ def pmix_to_list(pmix):
 
         for line in csv_reader:
             product_list.append((line[1], line[5]))
+
+pmix_to_list(pmix)
 
 if __name__ == '__main__':
     app.run()
